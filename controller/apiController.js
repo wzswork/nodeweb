26d1086df0df1e2fbe8a563bddedf2e2c90b4f71
wangzi <wzswork@hotmail.com>
2018-03-16 17:59:35 +0800

    build

--- controller/apiController.js
+++ controller/apiController.js
@@ -1,12 +1,17 @@
-
+import BlogModel from '../model/blog'
 
 class Api {
   constructor(){
 
   }
+  login(req, res, next){
+    
+  }
   getLabels(req, res, next){
-    console.log(1)
     res.send([{id:1, name:'javascript'}, {id:2, name:'生活文字'}, {id:3, name:'前端杂谈'}]);
+  }
+  getDigests(req, res, next){
+
   }
 }
 
